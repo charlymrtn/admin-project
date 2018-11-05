42f1ddae51a5a88b82e563f22d43d454f3770cd8
carlos martin <cjmar87@gmail.com>
2018-11-05 22:37:42 +0000

    modal de registrar/actualizar

--- resources/js/app.js
+++ resources/js/app.js
@@ -18,10 +18,6 @@ window.Vue = require('vue');
 //Vue.component('content-component', require('./components/ContentComponent.vue'));
 Vue.component('categoria-component', require('./components/CategoriaComponent.vue'));
 
-Vue.component('modal-eliminar-component', require('./components/modals/EliminarComponent.vue'));
-Vue.component('modal-nuevoeditar-component', require('./components/modals/NuevoEditarComponent.vue'));
-
-Vue.component('table-content-component', require('./components/tables/ContentComponent.vue'));
 
 const app = new Vue({
     el: '#app',
--- resources/js/components/CategoriaComponent.vue
+++ resources/js/components/CategoriaComponent.vue
@@ -13,7 +13,7 @@
           <div class="card">
               <div class="card-header">
                   <i class="fa fa-align-justify"></i> Categorías
-                  <button type="button" class="btn btn-secondary" data-toggle="modal" data-target="#modalNuevo">
+                  <button type="button" @click="abrirModal('categoria','registrar')" class="btn btn-secondary">
                       <i class="icon-plus"></i>&nbsp;Nuevo
                   </button>
               </div>
@@ -42,10 +42,10 @@
                       <tbody>
                           <tr v-for="categoria in categorias" :key="categoria.uuid">
                               <td>
-                                  <button type="button" class="btn btn-warning btn-sm" data-toggle="modal" data-target="#modalNuevo">
+                                  <button type="button" @click="abrirModal('categoria','actualizar',categoria)" class="btn btn-warning btn-sm">
                                     <i class="icon-pencil"></i>
                                   </button> &nbsp;
-                                  <button type="button" class="btn btn-danger btn-sm" data-toggle="modal" data-target="#modalEliminar">
+                                  <button type="button" class="btn btn-danger btn-sm">
                                     <i class="icon-trash"></i>
                                   </button>
                               </td>
@@ -89,9 +89,68 @@
           <!-- Fin ejemplo de tabla Listado -->
       </div>
 
-      <modal-nuevoeditar-component></modal-nuevoeditar-component>
+      <!--Inicio del modal agregar/actualizar-->
+      <div class="modal fade" tabindex="-1" :class="{'mostrar': modal}" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+          <div class="modal-dialog modal-primary modal-lg" role="document">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h4 class="modal-title" v-text="tituloModal"></h4>
+                      <button type="button" class="close" @click="cerrarModal()" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                      </button>
+                  </div>
+                  <div class="modal-body">
+                      <form action="" method="post" enctype="multipart/form-data" class="form-horizontal">
+                          <div class="form-group row">
+                              <label class="col-md-3 form-control-label" for="text-input">Nombre</label>
+                              <div class="col-md-9">
+                                  <input type="text" v-model="nombre" class="form-control" placeholder="Nombre de categoría">
+                                  <span class="help-block">(*) Ingrese el nombre de la categoría</span>
+                              </div>
+                          </div>
+                          <div class="form-group row">
+                              <label class="col-md-3 form-control-label" for="text-input">Descripción</label>
+                              <div class="col-md-9">
+                                  <input type="text" v-model="descripcion" class="form-control" placeholder="Descripción de la categoría">
+                              </div>
+                          </div>
+                      </form>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" @click="cerrarModal()" class="btn btn-secondary">Cerrar</button>
+                      <button type="button" v-if="tipoAccion==1" class="btn btn-primary">Guardar</button>
+                      <button type="button" v-if="tipoAccion==2" class="btn btn-primary">Actualizar</button>
+                  </div>
+              </div>
+              <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+      </div>
+      <!--Fin del modal-->
 
-      <modal-eliminar-component></modal-eliminar-component>
+      <!-- Inicio del modal Eliminar -->
+      <div class="modal fade" id="modalEliminar" tabindex="-1" role="dialog" aria-labelledby="myModalLabel" style="display: none;" aria-hidden="true">
+          <div class="modal-dialog modal-danger" role="document">
+              <div class="modal-content">
+                  <div class="modal-header">
+                      <h4 class="modal-title">Eliminar Categoría</h4>
+                      <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">×</span>
+                      </button>
+                  </div>
+                  <div class="modal-body">
+                      <p>Estas seguro de eliminar la categoría?</p>
+                  </div>
+                  <div class="modal-footer">
+                      <button type="button" class="btn btn-secondary" data-dismiss="modal">Cerrar</button>
+                      <button type="button" class="btn btn-danger">Eliminar</button>
+                  </div>
+              </div>
+              <!-- /.modal-content -->
+          </div>
+          <!-- /.modal-dialog -->
+      </div>
+      <!-- Fin del modal Eliminar -->
 
   </main>
 </template>
@@ -102,19 +161,56 @@
           return{
             nombre: '',
             descripcion: '',
-            categorias: []
+            categorias: [],
+            modal: 0,
+            tituloModal: '',
+            tipoAccion: 0
           }
         },
         methods: {
           listarCategoria(){
-            let me =this;
-            axios.get('/categorias')
-            .then(function (response){
+            let me = this;
+            axios.get('/categorias').then(function (response){
               me.categorias =response.data;
             })
             .catch(function (error){
               console.log(error);
             });
+          },
+          registrarCategoria(){
+
+          },
+          abrirModal(modelo, accion, data = []){
+            switch (modelo) {
+              case 'categoria':
+                {
+                  switch (accion) {
+                    case 'registrar':
+                    {
+                      this.modal = 1;
+                      this.tituloModal = 'Registrar Categoría';
+                      this.nombre = '';
+                      this.descripcion = '';
+                      this.tipoAccion = 1;
+                      break;
+                    }
+                    case 'actualizar':
+                    {
+
+                    }
+                  }
+                }
+              case 'producto':
+                {
+
+                }
+            }
+          },
+          cerrarModal(){
+            this.modal = 0;
+            this.tituloModal = "";
+            this.nombre = "";
+            this.descripcion = "";
           }
         },
         mounted() {
@@ -122,3 +218,15 @@
         }
     }
 </script>
+<style>
+  .modal-content{
+    width: 100% !important;
+    position: absolute !important;
+  }
+  .mostrar{
+    display: list-item !important;
+    opacity: 1 !important;
+    position: absolute !important;
+    background-color: #3c29297a !important;
+  }
+</style>
