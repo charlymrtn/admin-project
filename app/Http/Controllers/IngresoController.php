87fd670b00231d32b3455a45ea2f1d9cb0bb08a1
carlos martin <cjmar87@gmail.com>
2018-11-13 01:27:49 +0000

    continuamos con la funcion store faltan las validaciones

--- app/Http/Controllers/IngresoController.php
+++ app/Http/Controllers/IngresoController.php
@@ -6,6 +6,9 @@ use App\Models\Ingreso;
 use App\Models\DetalleIngreso;
 use Illuminate\Http\Request;
 
+use DB;
+use Carbon\Carbon;
+
 class IngresoController extends Controller
 {
   /**
@@ -15,7 +18,7 @@ class IngresoController extends Controller
    */
   public function index(Request $request)
   {
-      //if (!$request->ajax()) return redirect('/');
+      if (!$request->ajax()) return redirect('/');
 
       $buscar = $request->buscar;
       $criterio = $request->criterio;
@@ -63,30 +66,20 @@ class IngresoController extends Controller
 
       try {
 
-        $rules = [
-          'nombre' => 'required|string|min:5|max:100',
-          'email' => 'required|email',
-          'tipo_documento' => 'sometimes|string|min:3|max:20',
-          'num_documento' => 'required_with:tipo_documento|string|min:5|max:20',
-          'direccion' => 'sometimes|string|min:5|max:70',
-          'telefono' => 'sometimes|string|min:10|max:20',
-          'password' => 'required|string|min:3|max:10',
-          'usuario' => 'required|string|min:3|max:10',
-          'rol_uuid' => 'required|uuid|exists:roles,uuid'
-        ];
-
-        $this->validate($request,$rules);
-
         DB::beginTransaction();
 
-        $persona = Persona::create($request->only('nombre','tipo_documento','num_documento','direccion','telefono','email'));
+        $time = Carbon::now('America/Mexico_City')
 
-        $usuario = User::create([
-          'uuid' => $persona->uuid,
-          'usuario' => $request->usuario,
-          'password' => bcrypt($request->password),
-          'rol_uuid' => $request->rol_uuid,
-        ]);
+        $ingreso = new Ingreso();
+        $ingreso->proveedor_uuid = $request->proveedor_uuid;
+        $ingreso->usuario_uuid = Auth::user()->uuid;
+        $ingreso->tipo_comprobante = $request->tipo_comprobante;
+        $ingreso->serie_comprobante = $request->serie_comprobante;
+        $ingreso->num_comprobante = $request->num_comprobante;
+        $ingreso->impuesto = $request->impuesto;
+        $ingreso->total = $request->total;
+        $ingreso->estado = 'registrado';
+        $ingreso->fecha_ingreso = $time->toDateString();
 
         DB::commit();
 
