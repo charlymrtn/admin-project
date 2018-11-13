bd06b05c63617cbdbd4efbbb43255118bb096eb5
carlos martin <cjmar87@gmail.com>
2018-11-13 00:58:13 +0000

    creando controlador de ingreso

---
+++ app/Http/Controllers/IngresoController.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Ingreso;
+use App\Models\DetalleIngreso;
+use Illuminate\Http\Request;
+
+class IngresoController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Ingreso  $ingreso
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Ingreso $ingreso)
+    {
+        //
+    }
+}
